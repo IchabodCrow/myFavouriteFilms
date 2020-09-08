6dfd75ab9cd18a69fce21df1c4efec8c37c88b4d
ichabod <mrdoy@mail.ru>
2020-09-08 16:11:12 +0300

    fixLocaleValidation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import { Trans, useTranslation } from 'react-i18next'
+import axios from 'axios'
 
 import Main from "./components/MainPage/main";
 import insteadServer from "./services/insteadServer";
@@ -8,6 +9,15 @@ import { PrivateRoute } from "./helpers/PrivateRoute";
 import LoginPage from "./components/Login/LoginPage";
 
 insteadServer.prepareInitialData();
+
+const queriesTMDB = () => {
+  const apiKey = "api_key=09f30eef2a2368938501ec982e6bd612"
+    axios.get(`https://api.themoviedb.org/3/genre/movie/list?${apiKey}`)
+      .then( res => console.log('Response', res.data.genres))
+}
+
+queriesTMDB()
+
 
 function App () {
   const { i18n } = useTranslation();
