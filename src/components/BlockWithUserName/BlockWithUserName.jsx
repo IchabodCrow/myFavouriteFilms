669c753e21bb410e23d4456c6f491b9f7f36c75d
ichabod <mrdoy@mail.ru>
2020-09-15 14:38:40 +0300

    addLocale

--- src/components/BlockWithUserName/BlockWithUserName.jsx
+++ src/components/BlockWithUserName/BlockWithUserName.jsx
@@ -3,10 +3,12 @@ import { useHistory } from "react-router-dom";
 
 import Action from "../UI/Action";
 import { user } from "../../services/user";
+import { useTranslation } from "react-i18next";
 
 const BlockWithUserName = () => {
   const history = useHistory();
   const userName = user.userName();
+  const { t } = useTranslation()
 
   const handleClick = () => {
     user.logout();
@@ -15,8 +17,8 @@ const BlockWithUserName = () => {
 
   return (
     <div className="flex justify-end font-bold  text-purple-500 font-bold py-2 px-4 mx-6 rounded ">
-      <div className="text-3xl capitalize">hello, {userName} </div>
-      <Action onClick={handleClick} label={"logout"} />
+      <div className="text-3xl capitalize">{t("mainPage.greeting")}, {userName} </div>
+      <Action onClick={handleClick} label={t("description.logout")} />
     </div>
   );
 };
