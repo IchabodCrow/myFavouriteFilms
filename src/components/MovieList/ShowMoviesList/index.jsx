40abcab6fc2237f74cebe57eeaeef4c76c8b9167
ichabod <mrdoy@mail.ru>
2020-09-25 14:49:40 +0300

    addDisableOnButton

--- src/components/MovieList/ShowMoviesList/index.jsx
+++ src/components/MovieList/ShowMoviesList/index.jsx
@@ -7,8 +7,8 @@ export const ShowMoviesList = ({ movies, styleViews }) => {
     <div>
       {movies.results.map((movie, index) => {
         return (
-          <div className={"flex lg:max-w-full max-w-sm w-3/4 m-4 shadow-xl rounded-lg"} key={index}>
-            <img className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="List"/>
+          <div className={"flex lg:max-w-full max-w-sm w-3/4 m-10 shadow-xl rounded-lg"} key={index}>
+            <img className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden " src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="List"/>
             <div className={"w-3/4 rounded-b bg-white  p-4 flex flex-col justify-between leading-normal"}>
               <div className="mb-8">
                 <div className="text-gray-900 font-bold text-xl mb-2">
--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import classNames from 'classnames'
 
 import { ShowMoviesList } from "./ShowMoviesList";
 import { ShowMoviesBlocks } from './ShowMoviesBlocks'
@@ -6,12 +7,21 @@ import block from "resources/dial.png";
 import list from "resources/equal.png";
 
 export const MovieList = (props) => {
+ 
   const genresArr = props.genresId;
 
   const [state, setState] = useState({
     block: false,
     list: true,
   });
+
+  const activeBlock = classNames(
+    {'opacity-50': !state.block }
+  )
+
+  const activeList = classNames(
+    {'opacity-50': !state.list }
+  )
 
   useEffect(() => {
     props.moviesList({ genresArr: genresArr });
@@ -28,10 +38,10 @@ export const MovieList = (props) => {
   return (
     <div>
       <div className="flex flex-row-reverse">
-        <button onClick={changeDisplay} disabled={state.block}>
+        <button className={activeBlock} onClick={changeDisplay} disabled={state.block}>
           <img src={block} className="w-12 h-12 m-3" alt="Картинка!!!!" />
         </button>
-        <button onClick={changeDisplay} disabled={state.list}>
+        <button className={activeList} onClick={changeDisplay} disabled={state.list}>
           <img src={list} className="w-12 h-12 m-3" alt="Картинка!!!!" />
         </button>
       </div>
